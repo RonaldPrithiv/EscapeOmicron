3ac5a4f6c5c4af79feab02c5ae716738680c4520
Ronald Prithiv <ronaldprithiv98@gmail.com>
2022-01-14 01:14:14 +0000

    Levels

--- Assets/Scripts/Virus.cs
+++ Assets/Scripts/Virus.cs
@@ -5,15 +5,41 @@ using UnityEngine.UI;
 
 public class Virus : MonoBehaviour
 {
+    public float speed = 1;
+    public float distance;
+    private bool movingRight = true;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    public Transform groundDetection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+                sprite.flipX = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+                sprite.flipX = true;
+            }
+        }
     }
 }
